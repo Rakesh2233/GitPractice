3dbc3e936a84d090f339b1db172fd2accf1591d9
Rakesh2233 <rakeshpradhan909090@gmail.com>
2022-07-25 15:57:20 +0530

    1st

--- src/test/java/org/mavenExample/TestNgExample1Test.java
+++ src/test/java/org/mavenExample/TestNgExample1Test.java
@@ -4,20 +4,22 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class TestNgExample1Test {
-@Test
-public void practice1Test() {
-	Reporter.log("Tyss--> test1",true);
-}//asdfghjkl;qwertyuiopasdfghjk
-@Test 
-public void practice2Test() {
-	Reporter.log("Tyss--> test2",true);
-	//System.out.println("hiiii i am rakesh");
-	System.out.println("hiiii i am rajesh");//jenkin
-	
-	System.out.println("rakesh");//gh
-	System.out.println("rajesh");
-	System.out.println("git bash");
-	System.out.println("github to eclipse");
-	
-}
+	@Test
+	public void practice1Test() {
+		Reporter.log("Tyss--> test1",true);
+	}//asdfghjkl;qwertyuiopasdfghjk
+	@Test 
+	public void practice2Test() {
+		Reporter.log("Tyss--> test2",true);
+		//System.out.println("hiiii i am rakesh");
+		System.out.println("hiiii i am rajesh");//jenkin
+
+		System.out.println("rakesh");//gh
+		System.out.println("rajesh");
+		System.out.println("git bash");
+		System.out.println("github to eclipse");//did some change
+		
+		//System.out.println("changeds");
+		
+	}
 }
